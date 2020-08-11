c43a25b7658a9575f8e0c0f5cfaad9ac049ebe3d
Erin Lechler <erinlechler@gmail.com>
2020-08-11 16:07:31 -0500

    Trying to fig out event listener

--- js/app.js
+++ js/app.js
@@ -1,9 +1,35 @@
-const keyboard = document.getElementById('qwerty');
+                        //make sure you have this in program (from project review)
+//return a random phrase from an array
+// const getRandomPhraseAsArray = arr => {
+
+// }
+// //adds the letters of a string to the display
+// const addPhraseToDisplay = arr => {
+
+// }
+// //check if a letter is in the phrase
+// const checkLetter = button => {
+
+// }
+// //check if the game has been won or lost
+// const checkWin = () => {
+
+// }
+// //listen for the start game button to be pressed
+// startButton.addEventListener('click', () => {
+
+// });
+// //listen for the onscreen keyboard to be clicked
+// qwerty.addEventListener('click', e => {
+
+// });
+
+
+const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-const startButton = document.getElementsByClassName('.btn_reset');
+const startButton = document.getElementsByClassName('.btn__reset');
 const missed = 0;
-const overlay = document.getElementsByClassName('.main-container');
-    
+
 const phrases = [
     'Tim sees little green people',
     'Erin is the best mom ever',
@@ -12,5 +38,19 @@ const phrases = [
     'Shannon is the best kid on the block'
 ];
 
-startButton.addEventListener('click');
-  console.log('test');  
+
+
+
+
+//hides main container when you click anywhere, NOT JUST THE BUTTON START GAME
+const overlay = document.querySelectorAll('.start');
+    for (let i = 0 ; i < overlay.length; i++) {
+        overlay[i].addEventListener('click', function () {
+            overlay[i].style.display = 'none';
+            
+        });
+    }
+    
+
+
+
