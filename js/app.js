3eb2558589d3f726a7d40a62ac0042843a6541f7
Erin Lechler <erinlechler@gmail.com>
2020-08-11 14:46:30 -0500

    initial setup of var

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const keyboard = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const startButton = document.getElementsByClassName('.btn_reset');
+const missed = 0;
+const overlay = document.getElementsByClassName('.main-container');
+    
+const phrases = [
+    'Tim sees little green people',
+    'Erin is the best mom ever',
+    'Bailey has mad photog skills',
+    'Emerson has one thousand nicknames',
+    'Shannon is the best kid on the block'
+];
+
+startButton.addEventListener('click');
+  console.log('test');  
