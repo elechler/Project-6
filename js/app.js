cdfde2f0d20ce4ed96de86185b8bb0429c706909
Erin Lechler <erinlechler@gmail.com>
2020-08-12 15:39:12 -0500

    trying to add to display

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
                         //make sure you have this in program (from project review)
-//return a random phrase from an array
-// const getRandomPhraseAsArray = arr => {
-
-// }
 // //adds the letters of a string to the display
 // const addPhraseToDisplay = arr => {
 
@@ -20,22 +16,12 @@
 // qwerty.addEventListener('click', e => {
 
 // });
-
-
+//      START OF MY CODE....
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const missed = 0;
 
-const phrases = [
-    'Tim sees little green people',
-    'Erin is the best mom ever',
-    'Bailey has mad photog skills',
-    'Emerson has one thousand nicknames',
-    'Shannon is the best kid on the block'
-];
-
-//hides main container when you click START GAME
-
+                        //hides main container when you click START GAME
 const overlay = document.querySelectorAll('.start');
 const startButton = document.querySelectorAll('.btn__reset');  
     for (let i = 0 ; i < overlay.length; i++) {
@@ -44,6 +30,78 @@ const startButton = document.querySelectorAll('.btn__reset');
         });
     }
     
+const phrases = [
+    'Tim sees little green people',
+    'Erin is the best mom ever',
+    'Bailey has mad photog skills',
+    'Emerson has one thousand nicknames',
+    'Shannon is the best kid on the block'
+];
+
+//create a function that returns a random phrase from an array        
+// function getRandomPhraseAsArray(arr) {
+//     const randomNumber = Math.floor(Math.random() * arr.length );
+//     const randomPhrase = arr[randomNumber];
+//         function splitFunction() {
+//             let splitPhrase = randomPhrase.split("");
+//             console.log(splitPhrase);
+//         }
+//             splitFunction();   
+//         }
+// let test = getRandomPhraseAsArray(phrases);
+        
 
 
 
+
+function getRandomPhraseAsArray(arr) {
+    const randomNumber = Math.floor(Math.random() * arr.length );
+    const randomPhrase = arr[randomNumber];
+        function splitFunction() {
+            let splitPhrase = [randomPhrase.split("")];
+            console.log(splitPhrase);
+        }
+            splitFunction();   
+        }
+const splitPhrase = [getRandomPhraseAsArray(phrases)];
+      
+// const test = 'test';
+// document.querySelector('ul').innerHTML = test;
+
+
+// //adds the letters of a string to the display
+// const addPhraseToDisplay = arr => {
+
+// }
+
+//STUCK HERE--- NOTHING WORKS BELOW THIS
+function addPhraseToDisplay(arr) {
+    const listItems = document.getElementsByTagName('ul');
+    for (let i = 0; i < listItems.length; i +=1) {
+            listItems[i].textContent = listItems[i].textContent;
+            //document.querySelector('ul').innerHTML = listItems;
+            
+            };
+   }
+addPhraseToDisplay(splitPhrase);
+
+    
+    
+
+
+
+
+
+
+
+//CREATE A CHECK LETTER FUNCTION:
+ //check if a letter is in the phrase
+// const checkLetter = button => {
+// }
+
+// function checkLetter(buttonClicked) {
+
+// }
+
+// const checkLetter = document.getElementsByTagName('li');
+// console.log(checkLetter);
