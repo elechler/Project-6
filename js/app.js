449f24dd48f3271ef26ed01149ede5793a73a6aa
Erin Lechler <erinlechler@gmail.com>
2020-08-14 20:30:06 -0500

    working program

--- js/app.js
+++ js/app.js
@@ -1,68 +1,97 @@
-                          //make sure you have this in program (from project review)
-// //check if a letter is in the phrase
-// const checkLetter = button => {
-
-// }
-// //check if the game has been won or lost
-// const checkWin = () => {
-
-// }
-
-// //listen for the onscreen keyboard to be clicked
-// qwerty.addEventListener('click', e => {
-
-// });
-                                                //      START OF MY CODE....
-
-                                    //hides main container when you click START GAME
-const overlay = document.querySelectorAll('.start');
-const startButton = document.querySelectorAll('.btn__reset');  
-    for (let i = 0 ; i < overlay.length; i++) {
-        startButton[i].addEventListener('click', function () {
-            overlay[i].style.display = 'none';
-        });
-    }
-    
+                         
+///////////////////////////////////// //      START OF MY CODE....         
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const startButton = document.querySelector('a.btn__reset');  
+let missed = 0;
 const phrases = [
     'Tim sees little green people',
     'Erin is the best mom ever',
     'Bailey has mad photog skills',
     'Emerson has one thousand nicknames',
     'Shannon is the best kid on the block'
-];
-const missed = 0;
-
-                        //create a function that returns a random phrase and splits it
-const phrase = document.getElementById('phrase');
-
-function getRandomPhraseAsArray(arr){
-    const randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
-    const splitPhrase = randomPhrase.split("");
-    return splitPhrase;
-  } 
-const phraseArray = getRandomPhraseAsArray(phrases);
-                                
-                                //adds the letters of the random phrase to the display
-  function addPhraseToDisplay(arr) {
-    for (let i = 0; i < arr.length; i ++) {
-      let li = document.createElement('LI');
-      li.textContent = arr[i]; //does lower case change work?
-      const displayPhrase = document.querySelector('#phrase ul');
-      displayPhrase.appendChild(li);
-        if ( arr[i] !== ' ') {
-            li.classList.add('letter');
-            } else {
-                li.classList.add('space');
-            }
-    }
-}
-addPhraseToDisplay(phraseArray); 
-////////////////////////////////////////////////////////////////up above works
-//////////////////////////////////////////////////////down below is where you left off
-                                ////////listen for the onscreen keyboard to be clicked
-
-const qwerty = document.getElementById('qwerty');
-const buttons = document.querySelectorAll('button');
+    ];
+const overlay = document.getElementById('overlay');
+const scoreboard = document.querySelector('#scoreboard ol');
+const hearts = document.getElementsByTagName('img');
 let li = document.querySelector('#phrase ul');
 let letter = li.children;
+//////////////////////////////event listener START GAME and hide overlay
+startButton.addEventListener('click', () => {
+    overlay.style.display = 'none';
+});
+//////////////////////////////END HIDE OVERLAY
 
+///////////////////////////create getRandomPhraseAsArray 
+const getRandomPhraseAsArray = arr => {
+    let randomPhrase = arr[Math.floor(Math.random() * arr.length)];
+    let phraseOfChar = randomPhrase.split('');
+    return phraseOfChar;
+}
+const randomPhrase = getRandomPhraseAsArray(phrases);
+///////////////////////////END OF getRandomPhraseAsArray 
+
+//////////////////////////////////addPhraseToDisplay function
+const addPhraseToDisplay = arr => {
+    for (let i = 0; i < arr.length; i ++) {
+        let li = document.createElement('LI');
+        li.textContent = arr[i];
+        let displayPhrase = document.querySelector('#phrase ul');
+        displayPhrase.appendChild(li);
+        if ( arr[i] !== ' ') {
+            li.classList.add('letter');
+        } else {
+            li.classList.add('space')
+        }
+    }
+}
+addPhraseToDisplay(randomPhrase); 
+////////////////////////////////// END OF addPhraseToDisplay function
+
+////////////////////////////////// create a checkLetter function
+function checkLetter(button) {
+    let match = null;
+    for (let i = 0; i < letter.length; i ++) {
+        if (letter[i].textContent.toLowerCase() == button.textContent) {
+            letter[i].classList.add('show');
+            match = letter[i].textContent;
+        }
+    } return match;
+}
+////////////////////////////////// END of create a checkLetter function
+
+//////////////////////////////add an Event Listener to the keyboard
+qwerty.addEventListener('click', (event) => {
+    if (event.target.tagName == 'BUTTON') {
+        event.target.classList.add('chosen');
+        event.target.setAttribute('disabled', 'true')//???????
+        let letterFound = checkLetter(event.target);
+        if (letterFound == null){
+            missed++;
+            //hearts[missed - 1].src = 'images.lostHeart.png';//??
+            hearts[missed - 1].style.display = 'none';
+        }
+        checkWin();
+    }
+});
+///////////////////////////// END OF add an Event Listener to the keyboard
+
+///////////////////////////// create a checkWin funcion
+const checkWin = () => {
+    let letter = document.getElementsByClassName('letter');
+    let show = document.getElementsByClassName('show');
+    if (letter.length == show.length) {
+        overlay.className = 'win';
+        overlay.firstElementChild.textContent = 'You WON!!';
+        overlay.style.display = 'flex';
+        phrase.style.display = 'none';
+    } if ( missed > 4) {
+        overlay.className = 'lose';
+        overlay.firstElementChild.textContent = `I'm sorry, you LOST!!`;
+        overlay.style.display = 'flex';
+        phrase.style.display = 'none';
+    }
+}
+///////////////////////////// END OF create a checkWin funcion  
+  
+  
